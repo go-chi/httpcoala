f9e77b54fcc5dbf1b02a6b996ad3339f47a5421f
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-10-06 17:12:31 -0400

    Remove cruft, add some comments

--- httpcoala.go
+++ httpcoala.go
@@ -2,18 +2,14 @@ package httpcoala
 
 import (
 	"bytes"
-	"fmt"
-	"log"
 	"net/http"
 	"strings"
 	"sync"
 	"sync/atomic"
 )
 
-var (
-	counter int64 = 1
-)
-
+// Route middleware handler will coalesce multiple requests for the same URI
+// (and routed methods) to be processed as a single request.
 func Route(methods ...string) func(next http.Handler) http.Handler {
 	var requestsMu sync.Mutex
 	requests := make(map[request]*coalescer)
@@ -25,38 +21,43 @@ func Route(methods ...string) func(next http.Handler) http.Handler {
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			log.Println("broadcast handler..")
 
+			// Only route specified methods through the coalescer
 			if _, ok := methodTest[r.Method]; !ok {
 				next.ServeHTTP(w, r)
 				return
 			}
 
+			// Check if an existing request is in-flight
 			var reqKey = request{r.Method, r.URL.RequestURI()}
 			var cw *coalescer
 			var ww *writer
 			var exists bool
 
+			// TODO: code can be improved to synchronize adding a writer
+			// and waiting for the header to be written at first.
+			// The approach would be to make a type for the requests map
+			// with Get() and Set() methods, and remove the a request
+			// once the header of the coalescer has been sent.
 			requestsMu.Lock()
 			cw, exists = requests[reqKey]
 			if exists {
 				ww, exists = cw.AddWriter(w)
 			}
 			if !exists {
-				log.Println("!!!!! SET requests[reqKey] !!!!!") //, len(cw.writers))
 				cw = newCoalescer(w)
 				requests[reqKey] = cw
 			}
 			requestsMu.Unlock()
 
 			if ww != nil {
-				// existing request listening for the stuff..
-				log.Printf("waiting for existing request id:%d", ww.ID)
-
+				// Wait until stand-by request has flushed
 				<-ww.Flushed()
 				return
 			}
 
+			// First occurence of the request, setup a defer to flush the
+			// coalescer in case of panic and call next handler in the chain.
 			defer func() {
 				// Remove the request key from the map in case a request comes
 				// in while we're writing to the listeners
@@ -64,11 +65,11 @@ func Route(methods ...string) func(next http.Handler) http.Handler {
 				delete(requests, reqKey)
 				requestsMu.Unlock()
 
+				// Flushing the coalescer will broadcast to all writers
 				cw.Flush()
 				<-cw.Flushed()
 			}()
 
-			log.Println("sending request to next.ServeHTTP(cw,r)")
 			next.ServeHTTP(cw, r)
 		})
 	}
@@ -86,11 +87,8 @@ type coalescer struct {
 }
 
 func newCoalescer(w http.ResponseWriter) *coalescer {
-	id := atomic.LoadInt64(&counter)
-	atomic.AddInt64(&counter, 1)
-
 	return &coalescer{
-		writers: []*writer{newWriter(w, id)},
+		writers: []*writer{newWriter(w)},
 		header:  http.Header{},
 		bufw:    &bytes.Buffer{},
 	}
@@ -101,13 +99,11 @@ func (cw *coalescer) AddWriter(w http.ResponseWriter) (*writer, bool) {
 		return nil, false
 	}
 
-	id := atomic.LoadInt64(&counter)
-	atomic.AddInt64(&counter, 1)
-
+	// Synchronize writers and wroteHeader
 	cw.mu.Lock()
 	defer cw.mu.Unlock()
 
-	ww := newWriter(w, id)
+	ww := newWriter(w)
 	cw.writers = append(cw.writers, ww)
 	return ww, true
 }
@@ -117,7 +113,6 @@ func (cw *coalescer) Header() http.Header {
 }
 
 func (cw *coalescer) Write(p []byte) (int, error) {
-	log.Println("broadcastwriter: Write(), wroteHeader:", atomic.LoadUint32(&cw.wroteHeader))
 	if atomic.CompareAndSwapUint32(&cw.wroteHeader, 0, 1) {
 		cw.writeHeader(http.StatusOK)
 	}
@@ -132,22 +127,17 @@ func (cw *coalescer) WriteHeader(status int) {
 }
 
 func (cw *coalescer) writeHeader(status int) {
+	// Synchronize writers and wroteHeader
 	cw.mu.Lock()
 	defer cw.mu.Unlock()
 
-	log.Println("broadcastwriter: WriterHeader(), wroteHeader:", atomic.LoadUint32(&cw.wroteHeader))
-
-	log.Println("listeners...?", len(cw.writers))
-
 	for _, ww := range cw.writers {
-		log.Printf("=====> writeHeader() id:%d", ww.ID)
 		go func(ww *writer, status int, header http.Header) {
 			h := map[string][]string(ww.Header())
 			for k, v := range header {
 				h[k] = v
 			}
 			// h["X-Coalesce"] = []string{"hit"}
-			h["X-ID"] = []string{fmt.Sprintf("%d", ww.ID)}
 
 			ww.WriteHeader(status)
 			close(ww.wroteHeaderCh)
@@ -155,20 +145,13 @@ func (cw *coalescer) writeHeader(status int) {
 	}
 }
 
-// how does http streaming work...? can we broadcast streaming...?
-// best is to make a test case really..
-// but first, how does Flush() operate normally?
-// what happens to connections normally after a request..? etc.?
 func (cw *coalescer) Flush() {
 	if !atomic.CompareAndSwapUint32(&cw.flushed, 0, 1) {
 		return
 	}
-
 	if atomic.CompareAndSwapUint32(&cw.wroteHeader, 0, 1) {
 		cw.writeHeader(http.StatusOK)
 	}
-
-	log.Println("flushing..")
 
 	data := cw.bufw.Bytes()
 
@@ -177,8 +160,8 @@ func (cw *coalescer) Flush() {
 			// Block until the header has been written
 			<-ww.wroteHeaderCh
 
-			// Write the data to the original response writer
-			// and signal to the flush channel once complete.
+			// Write the data to the writer and signal the
+			// flush to be finished by closing the channel.
 			ww.Write(data)
 			close(ww.flushedCh)
 		}(ww, data)
@@ -195,16 +178,13 @@ type request struct {
 }
 
 type writer struct {
-	ID int64
 	http.ResponseWriter
 	wroteHeaderCh chan struct{}
 	flushedCh     chan struct{}
 }
 
-func newWriter(w http.ResponseWriter, id int64) *writer {
-	log.Printf("newWriter, id:%d", id)
+func newWriter(w http.ResponseWriter) *writer {
 	return &writer{
-		ID:             id,
 		ResponseWriter: w,
 		wroteHeaderCh:  make(chan struct{}, 1),
 		flushedCh:      make(chan struct{}, 1),
