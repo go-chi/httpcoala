6bae3219de03de20e047db2f46c4f18ada7441cb
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-10-06 17:29:11 -0400

    Update README, add example

---
+++ _example/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/goware/httpcoala"
+	"github.com/pressly/cji"
+	"github.com/zenazn/goji/web/middleware"
+)
+
+func main() {
+	r := cji.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(httpcoala.Route("GET"))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(100 * time.Millisecond) // expensive op
+		w.WriteHeader(200)
+		w.Write([]byte("hi"))
+	})
+
+	http.ListenAndServe(":1111", r)
+}
